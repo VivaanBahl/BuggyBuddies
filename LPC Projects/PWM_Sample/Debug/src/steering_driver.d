ac740627af55fff35496fe75af05c983be63470b
Vivaan Bahl <vivaanbahl@gmail.com>
2017-10-15 21:02:32 -0400

    added in PWM sample project

---
+++ LPC Projects/PWM_Sample/Debug/src/steering_driver.d
@@ -0,0 +1,1 @@
+src/steering_driver.o src/steering_driver.d: ../src/steering_driver.c
---
+++ LPC Projects/PWM_Sample/src/PWM_Sample.c
@@ -0,0 +1,73 @@
+/*
+===============================================================================
+ Name        : PWM_Sample.c
+ Author      : $(author)
+ Version     :
+ Copyright   : $(copyright)
+ Description : main definition
+===============================================================================
+*/
+
+#include "board.h"
+#include <stdio.h>
+
+// TODO: insert other include files here
+
+// TODO: insert other definitions and declarations here
+///**
+// * @brief	Handle interrupt from SysTick timer
+// * @return	Nothing
+// */
+void SysTick_Handler(void)
+{
+	Board_LED_Set(0, false);
+}
+
+/**
+ * @brief	Handle interrupt from 32-bit timer
+ * @return	Nothing
+ */
+void TIMER16_0_IRQHandler(void)
+{
+	if (Chip_TIMER_MatchPending(LPC_TIMER16_0, 1)) {
+		Chip_TIMER_ClearMatch(LPC_TIMER16_0, 1);
+		Board_LED_Toggle(0);
+	}
+}
+
+/**
+ * @brief	main routine for blinky example
+ * @return	Function should not exit.
+ */
+int main(void)
+{
+
+	SystemCoreClockUpdate();
+	Board_Init();
+
+	/* Enable and setup SysTick Timer at a periodic rate */
+	SysTick_Config(SystemCoreClock / 10);
+
+	/* Enable timer 1 clock */
+	Chip_TIMER_Init(LPC_TIMER16_0);
+
+	/* Timer setup for match and interrupt at TICKRATE_HZ */
+	Chip_TIMER_Reset(LPC_TIMER16_0);
+	LPC_TIMER16_0->MR[0] = 1000;
+	LPC_TIMER16_0->MR[3] = 10000;
+	LPC_TIMER16_0->MCR = 0;
+	Chip_TIMER_ResetOnMatchEnable(LPC_TIMER16_0, 3);
+	Chip_IOCON_PinMuxSet(LPC_IOCON, IOCON_PIO0_8, (IOCON_FUNC2 | IOCON_MODE_INACT));
+	LPC_TIMER16_0->PWMC = 0x1;
+	Chip_TIMER_Enable(LPC_TIMER16_0);
+
+
+	/* LEDs toggle in interrupt handlers */
+
+	volatile static int i = 0;
+	while (1) {
+		i++;
+	}
+
+	return 0;
+}
