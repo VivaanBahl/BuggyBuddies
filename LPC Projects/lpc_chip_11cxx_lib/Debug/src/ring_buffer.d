b40105440b87ed7f8651dbcfd289a44c6551a451
Kevin Lee <kevin18lee@gmail.com>
2017-10-19 16:04:10 -0400

    deleted more stuff

--- LPC Projects/lpc_chip_11cxx_lib/Debug/src/ring_buffer.d
+++
@@ -1,7 +0,0 @@
-src/ring_buffer.o src/ring_buffer.d: ../src/ring_buffer.c \
- /Users/kevinlee/BuggyBuddies/LPC\ Projects/lpc_chip_11cxx_lib/inc/ring_buffer.h \
- /Users/kevinlee/BuggyBuddies/LPC\ Projects/lpc_chip_11cxx_lib/inc/lpc_types.h
-
-/Users/kevinlee/BuggyBuddies/LPC\ Projects/lpc_chip_11cxx_lib/inc/ring_buffer.h:
-
-/Users/kevinlee/BuggyBuddies/LPC\ Projects/lpc_chip_11cxx_lib/inc/lpc_types.h:
