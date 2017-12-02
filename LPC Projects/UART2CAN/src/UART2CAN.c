c90dfc8e31708a5cbb3de9de403c251ffb01d9bb
Vivaan Bahl <vivaanbahl@gmail.com>
2017-12-02 18:07:05 -0500

    Added uart to can translation

---
+++ LPC Projects/UART2CAN/src/UART2CAN.c
@@ -0,0 +1,38 @@
+/*
+===============================================================================
+ Name        : UART2CAN.c
+ Author      : $(author)
+ Version     :
+ Copyright   : $(copyright)
+ Description : main definition
+===============================================================================
+*/
+
+#if defined (__USE_LPCOPEN)
+#if defined(NO_BOARD_LIB)
+#include "chip.h"
+#else
+#include "board.h"
+#endif
+#endif
+
+#include <cr_section_macros.h>
+
+// TODO: insert other include files here
+
+// TODO: insert other definitions and declarations here
+
+int main(void) {
+
+	SystemCoreClockUpdate();
+	Board_Init();
+    // TODO: insert code here
+
+    // Force the counter to be placed into memory
+    volatile static int i = 0 ;
+    // Enter an infinite loop, just incrementing a counter
+    while(1) {
+        i++ ;
+    }
+    return 0 ;
+}
