0ac37b50306ee1dbb9e4d867ccefe4a3f17a1d67
Vivaan Bahl <vivaanbahl@gmail.com>
2017-12-02 18:33:32 -0500

    Added in small sample test project to echo characters back at the user

--- LPC Projects/UART2CAN/src/UART2CAN.c
+++ LPC Projects/UART2CAN/src/UART2CAN.c
@@ -19,13 +19,35 @@
 #include <cr_section_macros.h>
 
 // TODO: insert other include files here
-
+void SysTick_Handler()
+{
+	Board_UARTPutSTR("hello world\n");
+}
 // TODO: insert other definitions and declarations here
 
 int main(void) {
 
 	SystemCoreClockUpdate();
 	Board_Init();
+//	SysTick_Config(SystemCoreClock / 10);
+
+	Chip_IOCON_PinMuxSet(LPC_IOCON, IOCON_PIO1_6, (IOCON_FUNC1 | IOCON_MODE_INACT));/* RXD */
+	Chip_IOCON_PinMuxSet(LPC_IOCON, IOCON_PIO1_7, (IOCON_FUNC1 | IOCON_MODE_INACT));/* TXD */
+
+	Chip_UART_Init(LPC_USART);
+	Chip_UART_SetBaud(LPC_USART, 115200);
+	Chip_UART_ConfigData(LPC_USART, (UART_LCR_WLEN8 | UART_LCR_SBS_1BIT));
+	Chip_UART_SetupFIFOS(LPC_USART, (UART_FCR_FIFO_EN | UART_FCR_TRG_LEV2));
+	Chip_UART_TXEnable(LPC_USART);
+
+	char c = 0;
+	while (c != 0x0A)
+	{
+		c = Board_UARTGetChar();
+		if (c == 0xFF) continue;
+		Board_UARTPutChar(c);
+	}
+	Board_UARTPutSTR("hello world\n");
     // TODO: insert code here
 
     // Force the counter to be placed into memory
