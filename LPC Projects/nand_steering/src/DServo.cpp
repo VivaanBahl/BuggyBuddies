33824519fa38c3c78067be2b89dc95628b2f9358
Tiffo Yu <tiffoyu@administorsMBP4.wv.cc.cmu.edu>
2018-03-25 19:46:15 -0400

    nand steering project

---
+++ LPC Projects/nand_steering/src/DServo.cpp
@@ -0,0 +1,431 @@
+#include "DServo.h"
+#include "chip.h"
+#include "board.h"
+#include "string.h"
+
+/* Transmit and receive ring buffers */
+STATIC RINGBUFF_T txring, rxring;
+
+/* Transmit and receive ring buffer sizes */
+#define UART_SRB_SIZE 128	/* Send */
+#define UART_RRB_SIZE 32	/* Receive */
+
+/* Transmit and receive buffers */
+static uint8_t rxbuff[UART_RRB_SIZE], txbuff[UART_SRB_SIZE];
+
+void DServoClass::begin(long baud){
+	//uint8_t key; //eventually declare these variables outside of begin, but for now leave it until this works
+	//int bytes;
+
+	SystemCoreClockUpdate(); //updates clock rate (why we need?)
+	Board_Init(); //initializes board
+	//Board_LED_Set(0, false);
+
+	/* Setup UART for 115.2K8N1 */
+	Chip_IOCON_PinMuxSet(LPC_IOCON, IOCON_PIO1_6, (IOCON_FUNC1 | IOCON_MODE_INACT));/* RXD */
+	Chip_IOCON_PinMuxSet(LPC_IOCON, IOCON_PIO1_7, (IOCON_FUNC1 | IOCON_MODE_INACT));/* TXD */
+	Chip_UART_Init(LPC_USART); //LPC_USART is pointer to UART pin that has been initialized (?)
+	Chip_UART_SetBaud(LPC_USART, baud); //set baud rate to 1 million by default (?) Currently does not set default and requires input
+	Chip_UART_ConfigData(LPC_USART, (UART_LCR_WLEN8 | UART_LCR_SBS_1BIT)); //configures parity bits (might need to change)
+	Chip_UART_SetupFIFOS(LPC_USART, (UART_FCR_FIFO_EN | UART_FCR_TRG_LEV2)); //sets up FIFOs
+	Chip_UART_TXEnable(LPC_USART); //enables connection to TX (transmit)
+
+	/* Before using the ring buffers, initialize them using the ring
+	   buffer init function */
+	RingBuffer_Init(&rxring, rxbuff, 1, UART_RRB_SIZE); //initialize transmit and receive buffers
+	RingBuffer_Init(&txring, txbuff, 1, UART_SRB_SIZE);
+
+	/*enables interrupts for UART*/
+	Chip_UART_IRQRBHandler(LPC_USART, &rxring, &txring);
+
+	/* Enable receive data and line status interrupt (do we need?)*/
+	Chip_UART_IntEnable(LPC_USART, (UART_IER_RBRINT | UART_IER_RLSINT)); //enables interrupt UART_IER_*
+
+	Chip_GPIO_SetPinDIROutput(LPC_GPIO, PIN_A_PORT, PIN_A); //Sets PIN_A to output pin
+
+	/* Configure pins as GPIO with pullup (BB for pinint)*/
+	//Chip_IOCON_PinMuxSet(LPC_IOCON, IOCON_PIN_A_ID,
+	//(IOCON_FUNC0 | IOCON_MODE_PULLUP | IOCON_DIGMODE_EN));
+
+	/* Configure channel interrupts as edge sensitive and both edge interrupt (BB for pinint)*/
+	//Chip_GPIO_SetupPinInt(LPC_GPIO, PIN_A_PORT, PIN_A, GPIO_INT_BOTH_EDGES);
+
+	/* Enable GPIO pin interrupts */
+	//Chip_GPIO_EnableInt(LPC_GPIO, PIN_A_PORT, (1 << PIN_A));
+
+	/* Enable wakeup for GPIO pins used in this example (BB for pinint)*/
+	//Chip_SYSCTL_EnableStartPin(0x04); // Pin A
+	//Chip_SYSCTL_ResetStartPin(0x04); // Pin A
+
+	/* Enable all clocks, even those turned off at wake power up (BB for pinint)*/
+	Chip_SYSCTL_SetWakeup(~(SYSCTL_SLPWAKE_IRCOUT_PD | SYSCTL_SLPWAKE_IRC_PD |
+	SYSCTL_SLPWAKE_FLASH_PD | SYSCTL_SLPWAKE_SYSOSC_PD | SYSCTL_SLPWAKE_SYSOSC_PD | SYSCTL_SLPWAKE_SYSPLL_PD));
+
+	/* Enable interrupt in the NVIC */
+	//NVIC_EnableIRQ(SERVOINT_NVIC_NAME);
+
+	Status_Return_Value = READ;
+
+}
+
+void DServoClass::setDirectionPin(unsigned char Control_ID){
+	Chip_GPIO_SetPinDIROutput(LPC_GPIO, PIN_A_PORT, Control_ID); //Sets PIN_A to output pin
+
+}
+
+
+//mode should be SERVO (set in header file to true); sets CW/CCW limits for specified servo ID
+unsigned int DServoClass::setMode(unsigned char ID, bool Dynamixel_Mode, unsigned int Dynamixel_CW_Limit,unsigned int Dynamixel_CCW_Limit){
+
+    Instruction_Packet_Array[0] = ID;
+    Instruction_Packet_Array[1] = SET_MODE_LENGTH;
+    Instruction_Packet_Array[2] = COMMAND_WRITE_DATA;
+    Instruction_Packet_Array[3] = EEPROM_CW_ANGLE_LIMIT_L;
+    if (Dynamixel_Mode == WHEEL) {                                    // Set WHEEL mode, this is done by setting both the clockwise and anti-clockwise angle limits to ZERO
+        Instruction_Packet_Array[4] = 0x00;
+        Instruction_Packet_Array[5] = 0x00;
+        Instruction_Packet_Array[6] = 0x00;
+        Instruction_Packet_Array[7] = 0x00;
+    }else {                                                             // Else set SERVO mode
+        Instruction_Packet_Array[4] = (unsigned char)(Dynamixel_CW_Limit);
+        Instruction_Packet_Array[5] = (unsigned char)((Dynamixel_CW_Limit & 0x0F00) >> 8);
+        Instruction_Packet_Array[6] = (unsigned char)(Dynamixel_CCW_Limit);
+        Instruction_Packet_Array[7] = (unsigned char)((Dynamixel_CCW_Limit & 0x0F00) >> 8);
+    }
+
+    /*clearRXbuffer();*/
+
+    transmitInstructionPacket();
+
+    if (Status_Return_Value == ALL){
+    readStatusPacket();
+        if (Status_Packet_Array[2] != 0){
+        return (Status_Packet_Array[2] | 0xF000);   // If there is a error Returns error value
+        }
+
+    }
+
+ }
+
+unsigned int DServoClass::servo(unsigned char ID,unsigned int Position,unsigned int Speed){
+
+    Instruction_Packet_Array[0] = ID;
+    Instruction_Packet_Array[1] = SERVO_GOAL_LENGTH;
+    Instruction_Packet_Array[2] = COMMAND_WRITE_DATA;
+    Instruction_Packet_Array[3] = RAM_GOAL_POSITION_L;
+    Instruction_Packet_Array[4] = (unsigned char)(Position);
+    Instruction_Packet_Array[5] = (unsigned char)((Position & 0x0F00) >> 8);
+    Instruction_Packet_Array[6] = (unsigned char)(Speed);
+    Instruction_Packet_Array[7] = (unsigned char)((Speed & 0x0F00) >> 8);
+
+    /*clearRXbuffer();*/
+
+    transmitInstructionPacket();
+
+
+    if (ID == 0XFE || Status_Return_Value != ALL ){     // If ID of FE is used no status packets are returned so we do not need to check it
+        return (0x00);
+    }else{
+        readStatusPacket();
+        if (Status_Packet_Array[2] == 0){               // If there is no status packet error return value
+            return (Status_Packet_Array[0]);            // Return SERVO ID
+        }else{
+            return (Status_Packet_Array[2] | 0xF000);   // If there is a error Returns error value
+        }
+    }
+
+}
+
+unsigned int DServoClass::readPosition(unsigned char ID){
+
+    Instruction_Packet_Array[0] = ID;
+    Instruction_Packet_Array[1] = READ_POS_LENGTH;
+    Instruction_Packet_Array[2] = COMMAND_READ_DATA;
+    Instruction_Packet_Array[3] = RAM_PRESENT_POSITION_L;
+    Instruction_Packet_Array[4] = READ_TWO_BYTE_LENGTH;
+
+    /*clearRXbuffer();*/
+
+    transmitInstructionPacket();
+    readStatusPacket();
+
+    if (Status_Packet_Array[2] == 0){               // If there is no status packet error return value
+        return Status_Packet_Array[4] << 8 | Status_Packet_Array[3];    // Return present position value
+    }else{
+        return (Status_Packet_Array[2] | 0xF000);                           // If there is a error Returns error value
+    }
+}
+
+
+
+unsigned int DServoClass::setBaudRate(unsigned char ID, long Baud){
+
+    Instruction_Packet_Array[0] = ID;
+    Instruction_Packet_Array[1] = SET_BD_LENGTH;
+    Instruction_Packet_Array[2] = COMMAND_WRITE_DATA;
+    Instruction_Packet_Array[3] = EEPROM_BAUD_RATE;
+
+    switch (Baud){
+        case 1000000:
+            Instruction_Packet_Array[4] = 0x01;
+            break;
+        case 2250000:
+            Instruction_Packet_Array[4] = 0xFA;
+            break;
+        case 2500000:
+            Instruction_Packet_Array[4] = 0xFB;
+            break;
+        case 3000000:
+            Instruction_Packet_Array[4] = 0xFC;
+            break;
+        default:
+        Instruction_Packet_Array[4] = (unsigned char)((2000000/Baud) - 1);
+    }
+
+
+    transmitInstructionPacket();
+
+    if (ID == 0XFE || Status_Return_Value != ALL ){     // If ID of FE is used no status packets are returned so we do not need to check it
+        return (0x00);
+    }else{
+        readStatusPacket();
+        if (Status_Packet_Array[2] == 0){               // If there is no status packet error return value
+            return (Status_Packet_Array[0]);            // Return SERVO ID
+        }else{
+            return (Status_Packet_Array[2] | 0xF000);   // If there is a error Returns error value
+        }
+    }
+}
+
+unsigned int DServoClass::setID(unsigned char ID, unsigned char New_ID){
+
+    Instruction_Packet_Array[0] = ID;
+    Instruction_Packet_Array[1] = SET_ID_LENGTH;
+    Instruction_Packet_Array[2] = COMMAND_WRITE_DATA;
+    Instruction_Packet_Array[3] = EEPROM_ID;
+    Instruction_Packet_Array[4] = New_ID;
+
+    transmitInstructionPacket();
+
+    if (ID == 0XFE || Status_Return_Value != ALL ){     // If ID of FE is used no status packets are returned so we do not need to check it
+        return (0x00);
+    }else{
+        readStatusPacket();
+        if (Status_Packet_Array[2] == 0){               // If there is no status packet error return value
+            return (Status_Packet_Array[0]);            // Return SERVO ID
+        }else{
+            return (Status_Packet_Array[2] | 0xF000);   // If there is a error Returns error value
+        }
+    }
+}
+
+
+
+unsigned int DServoClass::setStatusPaket(unsigned char  ID,unsigned char Set){
+
+    Instruction_Packet_Array[0] = ID;
+    Instruction_Packet_Array[1] = SET_RETURN_LEVEL_LENGTH;
+    Instruction_Packet_Array[2] = COMMAND_WRITE_DATA;
+    Instruction_Packet_Array[3] = EEPROM_RETURN_LEVEL;
+    Instruction_Packet_Array[4] = Set;
+
+    Status_Return_Value = Set;
+
+    transmitInstructionPacket();
+
+    if (ID == 0XFE || Status_Return_Value != ALL ){     // If ID of FE is used no status packets are returned so we do not need to check it
+        return (0x00);
+    }else{
+        readStatusPacket();
+        if (Status_Packet_Array[2] == 0){               // If there is no status packet error return value
+            return (Status_Packet_Array[0]);            // Return SERVO ID
+        }else{
+            return (Status_Packet_Array[2] | 0xF000);   // If there is a error Returns error value
+        }
+    }
+
+}
+
+unsigned int DServoClass::reset(unsigned char ID){
+
+    Instruction_Packet_Array[0] = ID;
+    Instruction_Packet_Array[1] = RESET_LENGTH;
+    Instruction_Packet_Array[2] = COMMAND_RESET;
+
+    transmitInstructionPacket();
+
+    if (ID == 0XFE || Status_Return_Value != ALL ){     // If ID of FE is used no status packets are returned so we do not need to check it
+        return (0x00);
+    }else{
+        readStatusPacket();
+        if (Status_Packet_Array[2] == 0){               // If there is no status packet error return value
+            return (Status_Packet_Array[0]);            // Return SERVO ID
+        }else{
+            return (Status_Packet_Array[2] | 0xF000);   // If there is a error Returns error value
+        }
+    }
+
+}
+
+void DServoClass::transmitInstructionPacket(void){                                   // Transmit instruction packet to Dynamixel
+
+
+	bool pin_a = Chip_GPIO_GetPinState(LPC_GPIO, PIN_A_PORT, PIN_A); //gets pin state
+	if (pin_a == false) //if pin is low, set state to high
+	{
+		Chip_GPIO_SetPinState(LPC_GPIO, PIN_A_PORT, PIN_A, true); //sets pin A to high
+	}
+
+/*NEW CODE STARTS HERE
+
+	unsigned char Temp_Instruction_Packet_Array[16]; //size of instruction packet + 2 headers
+	Temp_Instruction_Packet_Array[0] = HEADER;
+	Temp_Instruction_Packet_Array[1] = HEADER;
+	for (int i = 2; i < 16; i++)
+	{
+		Temp_Instruction_Packet_Array[i] = Instruction_Packet_Array[i-2];
+	}
+	uint32_t res = 0;
+	res = Chip_UART_SendRB(LPC_USART, &txring, Temp_Instruction_Packet_Array, 16);
+
+	for (int i = 0; i < 10000000; i++){}
+
+	unsigned int checksum_packet = Instruction_Packet_Array[0] + Instruction_Packet_Array[1] + Instruction_Packet_Array[2]; //start reading from index 3 of instruction packet
+
+	for (unsigned char i = 5; i <= Instruction_Packet_Array[3]; i++){
+	    	Chip_UART_SendRB(LPC_USART, &txring, &Temp_Instruction_Packet_Array[i], 1);    // Write Instruction & Parameters (if there are any) to serial
+	        checksum_packet += Instruction_Packet_Array[i];
+	}
+
+
+	Chip_GPIO_DisableInt(LPC_GPIO, PIN_A_PORT, (1 << PIN_A));
+    Checksum_Array[0] = ~checksum_packet & 0xFF;
+	Chip_UART_SendRB(LPC_USART, &txring, Checksum_Array, 1);
+
+//NEW CODE END*/
+
+/*This is what the new code replaces (starting from here)*/
+	Header_Array[0] = HEADER;
+	Header_Array[1] = HEADER;
+
+	for(int i = 0; i < 10; i++){
+		if ((Chip_UART_ReadLineStatus(LPC_USART) & UART_LSR_RDR) != 0) break;
+	}
+
+	Chip_UART_SendBlocking(LPC_USART, Header_Array, 2); //sending the header array
+	Chip_UART_SendBlocking(LPC_USART, Instruction_Packet_Array, 3); //sending the first 3 bytes in the Instruction Packet
+
+
+    unsigned int checksum_packet = Instruction_Packet_Array[0] + Instruction_Packet_Array[1] + Instruction_Packet_Array[2]; //start reading from index 3 of instruction packet
+
+    for (unsigned char i = 3; i <= Instruction_Packet_Array[1]; i++){
+    	Chip_UART_SendBlocking(LPC_USART, &Instruction_Packet_Array[i], 1);    // Write Instruction & Parameters (if there are any) to serial
+        checksum_packet += Instruction_Packet_Array[i];
+    }
+
+    //Chip_GPIO_DisableInt(LPC_GPIO, PIN_A_PORT, (1 << PIN_A));
+
+    Checksum_Array[0] = ~checksum_packet & 0xFF;
+    Chip_UART_SendBlocking(LPC_USART, Checksum_Array, 1); //sending the checksum array
+
+//This is where the new code replacement ends*/
+
+    while(UART_LSR_RDR == 0) {} //wait for TX data to be sent
+
+    bool pin_a1 = Chip_GPIO_GetPinState(LPC_GPIO, PIN_A_PORT, PIN_A); //gets pin state
+    if (pin_a1 == true) //if pin is high, set state to low
+    {
+    	Chip_GPIO_SetPinState(LPC_GPIO, PIN_A_PORT, PIN_A, false); //sets pin A to low
+    }
+
+    //Chip_GPIO_EnableInt(LPC_GPIO, PIN_A_PORT, (1 << PIN_A)); //enable interrupts again for pin A
+
+}
+
+unsigned int DServoClass::readStatusPacket(void){
+
+    unsigned char Counter = 0x00;
+    unsigned char First_Header = 0x00;
+    unsigned char Temp_Status_Packet_Array[2]; //size of status pkt incl. first two header indices
+    Status_Packet_Array[0] = 0x00;
+    Status_Packet_Array[1] = 0x00;
+    Status_Packet_Array[2] = 0x00;
+    Status_Packet_Array[3] = 0x00;
+
+    //unsigned int Time_Counter_Temp = Time_Counter + STATUS_PACKET_TIMEOUT;
+
+    /*Time_Counter = STATUS_PACKET_TIMEOUT + millis();                                    // Setup time out error, 50 ms + millis()
+
+    while(STATUS_FRAME_BUFFER >= _serial->available()){                                     // Wait for " header + header + frame length + error " RX data; need at
+                                                                                            // least 5 indices' worth of information before we may begin to read
+                                                                                            //STATUS_FRAME_BUFFER initialized to 5
+                                                                                            //_serial->available() checks the number of bytes of data in RX (received)
+
+        if (millis() >= Time_Counter){
+            return Status_Packet_Array[2] = B10000000;                                      // Return with Error if Serial data not received with in time limit
+        }
+    }*/
+
+    //pop off values on the receive ring buffer and stores in temp status packet array
+    /*Chip_UART_ReadBlocking(LPC_USART, Temp_Status_Packet_Array, 2);
+
+    if (Temp_Status_Packet_Array[0] == 0xFF && First_Header != 0xFF){
+    	First_Header = Temp_Status_Packet_Array[0];                                                 // Clear 1st header from RX buffer (should be 0xFF)
+    }else if (Temp_Status_Packet_Array[0] == -1){
+    	return Status_Packet_Array[2] = 0x80;                                      // Return with Error (b10000000 = 0x80) if two headers are not found
+    }
+
+    if(Temp_Status_Packet_Array[1] == 0xFF && First_Header == 0xFF){
+
+    	Status_Packet_Array[0] = Chip_UART_ReadBlocking(LPC_USART, Status_Packet_Array, 1);                                   // ID sent from Dynamixel
+    	Status_Packet_Array[1] = Chip_UART_ReadBlocking(LPC_USART, &Status_Packet_Array[1], 1);                                   // Frame Length of status packet
+    	Status_Packet_Array[2] = Chip_UART_ReadBlocking(LPC_USART, &Status_Packet_Array[2], 1);                                   // Error byte
+
+    	do{
+    		Status_Packet_Array[Counter + 3] = Chip_UART_ReadBlocking(LPC_USART, &Status_Packet_Array[Counter + 3], 1);          //set values in 3rd to nth index of Status Packet to corresponding
+    																						   //values in the temporary Status Packet Array
+    		Counter++;
+    	}while(Status_Packet_Array[1] > Counter);                           // Read Parameter(s) into array
+
+    	Status_Packet_Array[Counter + 4] = Chip_UART_ReadBlocking(LPC_USART, &Status_Packet_Array[Counter + 4], 1);                         // Read Check sum (trace through before verify is correct)
+
+	}else{
+		return Status_Packet_Array[2] = 0x80;                                      // Return with Error if two headers are not found
+	}*/
+
+    //pop off values on the receive ring buffer and stores in temp status packet array
+        Chip_UART_ReadBlocking(LPC_USART, Temp_Status_Packet_Array, 2);
+
+        if (Temp_Status_Packet_Array[0] == 0xFF && First_Header != 0xFF){
+        	First_Header = Temp_Status_Packet_Array[0];                                                 // Clear 1st header from RX buffer (should be 0xFF)
+        }else if (Temp_Status_Packet_Array[0] == -1){
+        	return Status_Packet_Array[2] = 0x80;                                      // Return with Error (b10000000 = 0x80) if two headers are not found
+        }
+
+        if(Temp_Status_Packet_Array[1] == 0xFF && First_Header == 0xFF){
+
+        	Status_Packet_Array[0] = Chip_UART_ReadBlocking(LPC_USART, &Status_Packet_Array[0], 1);                                   // ID sent from Dynamixel
+        	Status_Packet_Array[1] = Chip_UART_ReadBlocking(LPC_USART, &Status_Packet_Array[1], 1);                                   // Frame Length of status packet
+        	Status_Packet_Array[2] = Chip_UART_ReadBlocking(LPC_USART, &Status_Packet_Array[2], 1);                                   // Error byte
+
+        	do{
+        		Status_Packet_Array[Counter + 3] = Chip_UART_ReadBlocking(LPC_USART, &Status_Packet_Array[Counter + 3], 1);          //set values in 3rd to nth index of Status Packet to corresponding
+        																						   //values in the temporary Status Packet Array
+        		Counter++;
+        	}while(Status_Packet_Array[1] > Counter);                           // Read Parameter(s) into array
+
+        	Status_Packet_Array[Counter + 4] = Chip_UART_ReadBlocking(LPC_USART, &Status_Packet_Array[Counter + 4], 1);                         // Read Check sum (trace through before verify is correct)
+
+    	}else{
+    		return Status_Packet_Array[2] = 0x80;                                      // Return with Error if two headers are not found
+    	}
+}
+
+/*void DServoClass::clearRXbuffer(void){
+	unsigned char *clearRX_Dummy_Array[14];		//initialized to size of Instruction_Packet_Array bc at most that much info can be sent
+	Chip_UART_ReadBlocking(LPC_USART, clearRX_Dummy_Array, 14);
+
+
+}*/
+
+DServoClass DServo;
---
+++ LPC Projects/nand_steering/src/nand_steering.cpp
@@ -0,0 +1,101 @@
+/*
+===============================================================================
+ Name        : main.c
+ Author      : $(author)
+ Version     :
+ Copyright   : $(copyright)
+ Description : main definition
+===============================================================================
+*/
+
+#if defined (__USE_LPCOPEN)
+#if defined(NO_BOARD_LIB)
+#include "chip.h"
+#else
+#include "board.h"
+#include "DServo.h"
+#endif
+#endif
+
+#include <cr_section_macros.h>
+
+
+
+#define SERVO_ID 0x04               // ID of which we will set Dynamixel too
+#define SERVO_ControlPin 0x02       // Control pin of buffer chip, NOTE: this does not matter becasue we are not using a half to full contorl buffer.
+#define SERVO_SET_Baudrate 57600    // Baud rate speed which the Dynamixel will be set to (57600)
+#define CW_LIMIT_ANGLE 0x001        // lowest clockwise angle is 1, as when set to 0 it set servo to wheel mode
+#define CCW_LIMIT_ANGLE 0xFFF       // Highest anit-clockwise angle is 0XFFF, as when set to 0 it set servo to wheel mode
+
+//unsigned int pos = 0;
+
+/*void SERVOINT_IRQ_HANDLER (void) {
+	Chip_GPIO_ClearInts(LPC_GPIO, PIN_A_PORT, (1 << PIN_A));
+}*/
+
+int main(void) {
+
+#if defined (__USE_LPCOPEN)
+    // Read clock settings and update SystemCoreClock variable
+    SystemCoreClockUpdate();
+#if !defined(NO_BOARD_LIB)
+    // Set up and initialize all required blocks and
+    // functions related to the board hardware
+    Board_Init();
+    // Set the LED to the state of "On"
+    Board_LED_Set(0, true);
+#endif
+#endif
+
+    /*DServo.setDirectionPin(SERVO_ControlPin);
+
+      for(int b=1; b<0xFF; b++){
+        long Baudrate_BPS = 0;
+        Baudrate_BPS = 2000000/(b+1);
+        DServo.begin(Baudrate_BPS);
+        DServo.reset(0xFE);
+        for(int j = 0; j < 500000; j++){}
+      }
+      for(int j = 0; j < 1000000; j++){}
+
+      DServo.begin(57600);
+      DServo.setID(0xFE,SERVO_ID);
+      for(int j = 0; j < 500000; j++){}
+      DServo.setStatusPaket(SERVO_ID,READ);
+      DServo.setBaudRate(SERVO_ID,100000);
+      for(int j = 0; j < 500000; j++){}
+
+      DServo.begin(100000);
+      for(int j = 0; j < 500000; j++){}
+      DServo.setMode(SERVO_ID,SERVO,0x000,0xFFF);
+      for(int j = 0; j < 500000; j++){}
+      //Dynamixel.setMaxTorque(SERVO_ID,0x2FF);
+      //}*/
+
+    DServo.begin(SERVO_SET_Baudrate);                                    // We now need to set Ardiuno to the new Baudrate speed
+    DServo.setMode(SERVO_ID, SERVO, CW_LIMIT_ANGLE, CCW_LIMIT_ANGLE);    // set mode to SERVO and set angle limits
+
+    for(int j = 0; j < 1000000; j++){}
+
+
+    // Force the counter to be placed into memory
+    volatile static int i = 0 ;
+    // Enter an infinite loop, just incrementing a counter
+    while(1) {
+    	unsigned int pos = DServo.readPosition(SERVO_ID);
+    	for(int j = 0; j < 5000000; j++){}
+    	/*DServo.servo(SERVO_ID,0,0x100);  //  Move servo to max angle at max speed (1023)
+    	for(int j = 0; j < 2000000; j++){}
+
+    	//pos = DServo.readPosition(SERVO_ID);
+
+
+    	DServo.servo(SERVO_ID,0x0FF,0x100);   // Move servo to angle 1(0.088 degree) at speed 100
+    	for(int j = 0; j < 2000000; j++){}
+    	//pos = DServo.readPosition(SERVO_ID);*/
+
+
+        i++ ;
+    }
+    return 0;
+}
